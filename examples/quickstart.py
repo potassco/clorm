9c161276747cf0efb1a24b345991a66a61113ddd
David Rajaratnam <daver@cse.unsw.edu.au>
2018-12-06 20:43:09 +1100

    Added a first quick start example. But want to change it to make it more compelling.

---
+++ examples/quickstart.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+import sys,os
+PARENT_DIR=os.path.abspath("..")
+sys.path.insert(0,PARENT_DIR)
+
+from clorm import monkey; monkey.replace_control() # must call this before
+                                                   # importing clingo
+
+from clorm import Predicate, ConstantField, IntegerField, FactBase
+from clorm import ph1_
+
+from clingo import Control, parse_program
+
+#------------------------------------------------------
+# A Logic program
+#------------------------------------------------------
+
+logic_program='''
+flies(X) :- bird(X), not penguin(X).
+'''
+
+#------------------------------------------------------
+# The data model
+#------------------------------------------------------
+class Bird(Predicate):
+    name=ConstantField()
+
+class Penguin(Predicate):
+    name=ConstantField()
+
+class Flies(Predicate):
+    name=ConstantField()
+
+#------------------------------------------------------
+#
+#------------------------------------------------------
+
+def on_model(model):
+    # To show that the Model wrapper copies the original
+    for a in model.symbols(atoms=True): print("ATOM: {}".format(a))
+    if model.contains(Bird("tweety")): print("YES")
+
+    print("========== MODEL: START ==============")
+    fb=FactBase([Flies.name])
+    fb.add(model.facts([Bird,Penguin,Flies], atoms=True))
+
+    query=fb.select(Flies).where(Flies.name == ph1_)
+    for b in fb.select(Bird).get():
+        if len(list(query.get(b.name))):
+            print("{} is a flying bird".format(b.name))
+        else:
+            print("{} is a non-flying bird".format(b.name))
+    print("========== MODEL: END ==============")
+
+#------------------------------------------------------
+#
+#------------------------------------------------------
+
+def main():
+    ctrl = Control()
+    print("CONTROL: {}".format(ctrl))
+    with ctrl.builder() as b:
+        parse_program(logic_program, lambda stmt: b.add(stmt))
+
+    inputs=FactBase([Penguin.name])
+    f1=Bird("tweety")
+    f2=Bird("tux")
+    f3=Penguin("tux")
+    inputs.add([f1,f2,f3])
+
+    ctrl.add_facts(inputs)
+    ctrl.ground([("base",[])])
+    ctrl.solve(on_model=on_model)
+
+#------------------------------------------------------------------------------
+# main
+#------------------------------------------------------------------------------
+if __name__ == "__main__":
+    main()
+
+
