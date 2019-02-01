ba19f730b6c5e2191f2d086dcf2bea6ca6a1cead
David Rajaratnam <daver@cse.unsw.edu.au>
2019-02-01 14:25:03 +1100

    Updated manifest and setup files

--- setup.py
+++ setup.py
@@ -13,12 +13,12 @@ def read(fname):
 
 setup(
     name="CLORM",
-    version="0.3.2",
+    version="0.3.3",
     author="David Rajaratnam",
     author_email="daver@gemarex.com.au",
     description="Clingo ORM (CLORM) provides a ORM interface for interacting with the Clingo Answer Set Programming (ASP) solver",
     license="MIT",
     url="https://github.com/daveraja/clorm",
-    packages=["clorm","tests"],
+    packages=["clorm","clorm.lib"],
     long_description=read("README.rst"),
 )
