534d0fc79389a03f1789fec405dea4d12c40f9dd
David Rajaratnam <daver@cse.unsw.edu.au>
2019-03-18 21:33:15 +1100

    Update documentation version number

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2018, David Rajaratnam'
 author = 'David Rajaratnam'
 
 # The short X.Y version
-version = '0.4.3'
+version = '0.4.4'
 # The full version, including alpha/beta/rc tags
 release = 'alpha'
 
