a4976d5fa9f9781ac36a0ae3af08c28ba565142f
David Rajaratnam <daver@cse.unsw.edu.au>
2019-06-03 12:10:31 +1000

    Increment version number

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2018, David Rajaratnam'
 author = 'David Rajaratnam'
 
 # The short X.Y version
-version = '0.5.0'
+version = '0.5.1'
 # The full version, including alpha/beta/rc tags
 release = 'alpha'
 
