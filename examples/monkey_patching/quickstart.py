4317e86b6ca2b751c18c1a1e0d2e0d3511492c90
David Rajaratnam <daver@gemarex.com.au>
2019-11-12 14:21:51 +1100

    Added monkey patching example
    Removed the monkey patching of clingo.Control from the main quickstart example
    and copied it to a separate directory.

---
+++ examples/monkey_patching/quickstart.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+#--------------------------------------------------------------------------
+# Clorm can "monkey patch" the official clingo library. This will replace the
+# clingo.Control class with clorm.clingo.Control.
+# --------------------------------------------------------------------------
+
+from clorm import monkey; monkey.patch() # must call this before importing clingo
+
+from clorm import Predicate, ConstantField, IntegerField, FactBase
+from clorm import ph1_
+
+from clingo import Control # Import clingo.Control instead of clorm.clingo.Control
+
+
+ASP_PROGRAM="quickstart.lp"
+
+#--------------------------------------------------------------------------
+# Define a data model - we only care about defining the input and output
+# predicates.
+#--------------------------------------------------------------------------
+
+class Driver(Predicate):
+    name=ConstantField
+
+class Item(Predicate):
+    name=ConstantField
+
+class Assignment(Predicate):
+    item=ConstantField
+    driver=ConstantField
+    time=IntegerField
+
+
+#--------------------------------------------------------------------------
+#
+#--------------------------------------------------------------------------
+
+def main():
+    # Create a Control object that will unify models against the appropriate
+    # predicates. Then load the asp file that encodes the problem domain.
+    ctrl = Control(unifier=[Driver,Item,Assignment])
+    ctrl.load(ASP_PROGRAM)
+
+    # Dynamically generate the instance data
+    drivers = [ Driver(name=n) for n in ["dave", "morri", "michael" ] ]
+    items = [ Item(name="item{}".format(i)) for i in range(1,6) ]
+    instance = FactBase(drivers + items)
+
+    # Add the instance data and ground the ASP program
+    ctrl.add_facts(instance)
+    ctrl.ground([("base",[])])
+
+    # Generate a solution - use a call back that saves the solution
+    solution=None
+    def on_model(model):
+        nonlocal solution
+        solution = model.facts(atoms=True)
+
+    ctrl.solve(on_model=on_model)
+    if not solution:
+        raise ValueError("No solution found")
+
+    # Do something with the solution - create a query so we can print out the
+    # assignments for each driver.
+
+    #    query=solution.select(Assignment).where(lambda x,o: x.driver == o)
+    query=solution.select(Assignment).where(Assignment.driver == ph1_).order_by(Assignment.time)
+
+    for d in drivers:
+        assignments = query.get(d.name)
+        if not assignments:
+            print("Driver {} is not working today".format(d.name))
+        else:
+            print("Driver {} must deliver: ".format(d.name))
+            for a in assignments:
+                print("\t Item {} at time {}".format(a.item, a.time))
+
+#------------------------------------------------------------------------------
+# main
+#------------------------------------------------------------------------------
+if __name__ == "__main__":
+    main()
+
