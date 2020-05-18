2895606aead907e47237eee863b33a4770ac4ddf
David Rajaratnam <daver@gemarex.com.au>
2020-05-19 09:30:20 +1000

    Update version number to a non-release build dated 20200519

--- clorm/__init__.py
+++ clorm/__init__.py
@@ -1,6 +1,6 @@
 from .orm import *
 
-__version__ = "1.0.2"
+__version__ = "1.0.2.20200519"
 __author__ = "David Rajaratnam"
 __email__ = "daver@gemarex.com.au"
 __copyright__ = "Copyright (c) 2018 David Rajaratnam"
