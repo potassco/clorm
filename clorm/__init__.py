34e4784dcb822387b5f1e7886622419009a36ae3
David Rajaratnam <daver@gemarex.com.au>
2020-11-11 16:08:20 +1100

    Increment version number for the next release 1.1.0

--- clorm/__init__.py
+++ clorm/__init__.py
@@ -1,6 +1,6 @@
 from .orm import *
 
-__version__ = "1.0.4"
+__version__ = "1.1.0"
 __author__ = "David Rajaratnam"
 __email__ = "daver@gemarex.com.au"
 __copyright__ = "Copyright (c) 2018 David Rajaratnam"
