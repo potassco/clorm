075b7f025eff23bd8e1a63cb270a67c3c064e387
David Rajaratnam <daver@gemarex.com.au>
2020-11-17 13:22:23 +1100

    Fixed combine_fields example so that it returns a normal exit code
    The exit codes returned by clingo do not follow normal unix standards and
    instead follow the SAT competion. By breaking out of the loop early in the
    example it changed the exit code.

--- runexamples.sh
+++ runexamples.sh
@@ -85,4 +85,4 @@ run_python examples/monkey_patching/quickstart.py
 run_clingo examples/quickstart/embedded_quickstart.lp
 run_python examples/quickstart/quickstart.py
 
-#run_python examples/performance_tests/performance_comparison.py
+run_clingo examples/combine_fields/combine_fields.lp
