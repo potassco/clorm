e5d946622d1c7b0949d91bd7c8154ed86cc3be73
David Rajaratnam <daver@gemarex.com.au>
2021-07-28 22:37:39 +1000

    Increment bug-fix version number 1.2.1

--- clorm/__init__.py
+++ clorm/__init__.py
@@ -1,6 +1,6 @@
 from .orm import *
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 __author__ = "David Rajaratnam"
 __email__ = "daver@gemarex.com.au"
 __copyright__ = "Copyright (c) 2018 David Rajaratnam"
