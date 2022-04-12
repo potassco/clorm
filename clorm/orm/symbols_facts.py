55cd56c93e0b817668d30e30a59b5601dfc37fc2
FlorianFischer <florian91.fischer@gmail.com>
2022-04-12 21:39:04 +0200

    In symbols_facts.py, refactor to remove duplicate code

--- clorm/orm/symbols_facts.py
+++ clorm/orm/symbols_facts.py
@@ -10,12 +10,13 @@
 # ------------------------------------------------------------------------------
 
 import itertools
-from typing import Any, Iterable, Union
+from typing import Iterable, Iterator, Optional, Union
 from collections import abc, defaultdict
 
 import clingo
 import clingo.ast as clast
 from .core import *
+from .core import AnySymbol
 from .noclingo import SymbolMode, SymbolType, Function, Number, String
 from .factbase import *
 from .core import get_field_definition, PredicatePath, kwargs_check_keys, \
@@ -77,24 +78,7 @@ class Unifier(object):
     def add_predicate(self,predicate):
         self._add_predicates([predicate])
 
-    def unify_symbol(self,sym,*,raise_nomatch=False):
-        known_names = set([name for _, name in self._pgroups.keys()])
-        sym_name = sym.name
-        if sym_name in known_names:
-            sym_args = sym.arguments
-            for pred in self._pgroups[(len(sym_args),sym_name)]:
-                instance = pred._unify(sym, sym_args, sym_name)
-                if instance is not None:
-                    return instance
-        if raise_nomatch:
-            raise UnifierNoMatchError(
-                f"Cannot unify symbol '{sym}' to predicates in {self._predicates}",
-                sym, self._predicates)
-        return None
-
-    def unify(self,symbols,*,factbase=None,raise_nomatch=False):
-        fb=FactBase() if factbase is None else factbase
-        facts = []
+    def iter_unify(self, symbols: Iterable[AnySymbol], raise_nomatch: bool) -> Iterator[Predicate]:
         known_names = set([name for _, name in self._pgroups.keys()])
         for sym in symbols:
             sym_name = sym.name
@@ -104,13 +88,19 @@ class Unifier(object):
                 for pred in self._pgroups[(len(sym_args),sym_name)]:
                     instance = pred._unify(sym, sym_args, sym_name)
                     if instance is not None:
-                        facts.append(instance)
+                        yield instance
                         break
             if raise_nomatch and instance is None:
                 raise UnifierNoMatchError(
                     f"Cannot unify symbol '{sym}' to predicates in {self._predicates}",
                     sym, self._predicates)
-        fb.add(facts)
+
+    def unify_symbol(self, sym: AnySymbol, *, raise_nomatch: bool=False) -> Optional[Predicate]:
+        return next(self.iter_unify([sym], raise_nomatch), None)
+
+    def unify(self, symbols: Iterable[AnySymbol], *, factbase: Optional[FactBase]=None, raise_nomatch: bool=False) -> FactBase:
+        fb=FactBase() if factbase is None else factbase
+        fb.add(list(self.iter_unify(symbols, raise_nomatch)))
         return fb
 
 #------------------------------------------------------------------------------
@@ -119,28 +109,7 @@ class Unifier(object):
 # ------------------------------------------------------------------------------
 
 def _unify(predicates, symbols):
-    # To make things a little more efficient use the arity-name signature as a
-    # filter. However, Python doesn't have a built in multidict class, and I
-    # don't want to add a dependency to an external library just for one small
-    # feature, so implement a simple structure here.
-    types = defaultdict(list)
-    for cls in predicates:
-        types[(len(cls.meta), cls.meta.name)].append(cls)
-    known_names = set([name for _, name in types.keys()])
-    
-    # Loop through symbols and yield when we have a match
-    for raw in symbols:
-        raw_name = raw.name
-        if raw_name not in known_names:
-            continue
-        raw_args = raw.arguments
-        classes = types[(len(raw_args), raw_name)]
-        if not classes: continue
-        for cls in classes:
-            f = cls._unify(raw, raw_args, raw_name)
-            if f is not None:
-                yield f
-                break
+    return Unifier(predicates).iter_unify(symbols, raise_nomatch=False) 
 
 
 #------------------------------------------------------------------------------
