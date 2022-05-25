89108aad1f6f3e65b31fcb235c9341eed4510121
FlorianFischer <florian91.fischer@gmail.com>
2022-05-25 23:33:01 +0200

    add mypy test suite to test type annotations if mypy is installed

---
+++ tests/test_mypy.py
@@ -0,0 +1,159 @@
+from pathlib import Path
+import re
+import unittest
+import clorm
+
+def eq_(a, b, msg=None):
+    """Assert a == b, with repr messaging on failure."""
+    assert a == b, msg or "%r != %r" % (a, b)
+
+__all__=['MypyTestCase']
+
+
+def mypy_installed():
+    try:
+        import mypy
+    except ModuleNotFoundError:
+        return False
+    return True
+
+@unittest.skipIf(not mypy_installed(), reason="Test can just we executed if mypy is installed")
+class MypyTestCase(unittest.TestCase):
+    """TestCase for running a file with mypy and validate the revealed type by mypy"""
+
+    def mypy_runner(self, cachedir):
+        from mypy import api
+
+        def run(path):
+            args = ["--cache-dir", cachedir]
+            args.append(path)
+            return api.run(args)
+
+        return run
+    
+    def test_files(self):
+        """test wether revealed types by mypy are equal to expected type"""
+        # based on sqlalchemy
+        path =  Path("tests", "test_mypy_query.py")
+        expected_messages = []
+        
+        mypy_runner = self.mypy_runner(str(Path(clorm.__file__).parent.parent / ".mypy_cache"))
+        filename = path.name
+        expected_re = re.compile(r"\s*# EXPECTED(_MYPY)?(_RE)?(_TYPE)?: (.+)")
+        with open(path) as file_:
+            current_assert_messages = []
+            for num, line in enumerate(file_, 1):
+                m = expected_re.match(line)
+                if m:
+                    is_mypy = bool(m.group(1))
+                    is_re = bool(m.group(2))
+                    is_type = bool(m.group(3))
+
+                    expected_msg = re.sub(r"# noqa[:]? ?.*", "", m.group(4))
+                    if is_type:
+                        if not is_re:
+                            # the goal here is that we can cut-and-paste
+                            # from vscode -> pylance into the
+                            # EXPECTED_TYPE: line, then the test suite will
+                            # validate that line against what mypy produces
+                            expected_msg = re.sub(
+                                r"([\[\]])",
+                                lambda m: rf"\{m.group(0)}",
+                                expected_msg,
+                            )
+
+                            # note making sure preceding text matches
+                            # with a dot, so that an expect for "Select"
+                            # does not match "TypedSelect"
+                            expected_msg = re.sub(
+                                r"([\w_]+)",
+                                lambda m: rf"(?:.*\.)?{m.group(1)}\*?",
+                                expected_msg,
+                            )
+
+                            expected_msg = re.sub(
+                                "List", "builtins.list", expected_msg
+                            )
+
+                            expected_msg = re.sub(
+                                r"(int|str|float|bool)",
+                                lambda m: rf"builtins.{m.group(0)}\*?",
+                                expected_msg,
+                            )
+                            # expected_msg = re.sub(
+                            #     r"(Sequence|Tuple|List|Union)",
+                            #     lambda m: fr"typing.{m.group(0)}\*?",
+                            #     expected_msg,
+                            # )
+
+                        is_mypy = is_re = True
+                        expected_msg = f'Revealed type is "{expected_msg}"'
+                    current_assert_messages.append(
+                        (is_mypy, is_re, expected_msg.strip())
+                    )
+                elif current_assert_messages:
+                    expected_messages.extend(
+                        (num, is_mypy, is_re, expected_msg)
+                        for (
+                            is_mypy,
+                            is_re,
+                            expected_msg,
+                        ) in current_assert_messages
+                    )
+                    current_assert_messages[:] = []
+
+        result = mypy_runner(str(path))
+
+        if expected_messages:
+            eq_(result[2], 1, msg=result)
+
+            output = []
+
+            raw_lines = result[0].split("\n")
+            while raw_lines:
+                e = raw_lines.pop(0)
+                if re.match(r".+\.py:\d+: error: .*", e):
+                    output.append(("error", e))
+                elif re.match(
+                    r".+\.py:\d+: note: +(?:Possible overload|def ).*", e
+                ):
+                    while raw_lines:
+                        ol = raw_lines.pop(0)
+                        if not re.match(r".+\.py:\d+: note: +def \[.*", ol):
+                            break
+                elif re.match(
+                    r".+\.py:\d+: note: .*(?:perhaps|suggestion)", e, re.I
+                ):
+                    pass
+                elif re.match(r".+\.py:\d+: note: .*", e):
+                    output.append(("note", e))
+
+            for num, is_mypy, is_re, msg in expected_messages:
+                msg = msg.replace("'", '"')
+                prefix = "[SQLAlchemy Mypy plugin] " if not is_mypy else ""
+                for idx, (typ, errmsg) in enumerate(output):
+                    if is_re:
+                        if re.match(
+                            rf".*{filename}\:{num}\: {typ}\: {prefix}{msg}",  # noqa: E501
+                            errmsg,
+                        ):
+                            break
+                    elif (
+                        f"{filename}:{num}: {typ}: {prefix}{msg}"
+                        in errmsg.replace("'", '"')
+                    ):
+                        break
+                else:
+                    continue
+                del output[idx]
+
+            if output:
+                print(f"{len(output)} messages from mypy were not consumed:")
+                print("\n".join(msg for _, msg in output))
+                assert False, "errors and/or notes remain, see stdout"
+
+        else:
+            if result[2] != 0:
+                print(result[0])
+
+            eq_(result[2], 0, msg=result)
---
+++ tests/test_mypy_query.py
@@ -0,0 +1,181 @@
+from typing import Tuple
+from typing_extensions import reveal_type
+from clorm import FactBase, Predicate
+from clorm.orm._queryimpl import UnGroupedQuery, GroupedQuery
+from clorm.orm.query import QuerySpec, basic_join_order
+
+
+# this code is just to test type annotations with mypy and will not work when running with python
+
+class Customer(Predicate):
+    cid: int
+    name: str
+
+class Sale(Predicate):
+    sid: int
+    cid: int
+    item: str
+
+class Address(Predicate):
+    aid: int
+    customer: Customer
+
+
+def test_query_ungrouped_endpoints() -> None:
+    fb = FactBase()
+    query1 = fb.query(Sale, Customer)
+
+    # EXPECTED_TYPE: UnGroupedQuery[Tuple[Sale, Customer]]
+    reveal_type(query1)
+
+    # EXPECTED_TYPE: UnGroupedQuery[Tuple[Address, Sale, Customer]]
+    reveal_type(fb.query(Address, Sale, Customer))
+
+    # fallback to Any if query is called with more than 5 arguments
+    # EXPECTED_TYPE: UnGroupedQuery[Any]
+    reveal_type(fb.query(Address, Sale, Customer, Customer, Customer, Customer))
+    
+    # EXPECTED_TYPE: Generator[Tuple[Sale, Customer], None, None]
+    reveal_type(query1.all())
+
+    # EXPECTED_TYPE: Generator[Sale, None, None]
+    reveal_type(fb.query(Sale).all())
+
+    # EXPECTED_TYPE: Tuple[Sale, Customer]
+    reveal_type(query1.first())
+
+    # EXPECTED_TYPE: Tuple[Sale, Customer]
+    reveal_type(query1.singleton())
+
+    # EXPECTED_TYPE: int
+    reveal_type(query1.count())
+
+    # EXPECTED_TYPE: int
+    reveal_type(query1.delete())
+
+
+
+def test_query_ungrouped_select() -> None:
+    fb = FactBase()
+    query1 = fb.query()
+    
+    # EXPECTED_TYPE: UnGroupedQuery[Any]
+    reveal_type(query1)
+
+    s1 = query1.select(Sale.item, Customer)
+    # EXPECTED_TYPE: UnGroupedQuery[Tuple[str, Customer]]
+    reveal_type(s1)
+
+    s2 = query1.select(Sale.cid)
+    # EXPECTED_TYPE: UnGroupedQuery[int]
+    reveal_type(s2)
+
+    s3 = query1.select(Sale.cid, Customer.name, Address, Address.customer)
+    # EXPECTED_TYPE: UnGroupedQuery[Tuple[int, str, Address, Customer]]
+    reveal_type(s3)
+
+    # fallback to Any if select is called with more than 5 arguments
+    s4 = query1.select(Sale.cid, Sale.cid, Sale.cid, Sale.cid, Sale.cid, Sale.cid)
+    # EXPECTED_TYPE: UnGroupedQuery[Any]
+    reveal_type(s4)
+
+
+def test_query_grouped_select() -> None:
+    fb = FactBase()
+    query1 = fb.query(Sale, Customer)
+
+    # EXPECTED_TYPE: GroupedQuery[int, Tuple[Sale, Customer]]
+    reveal_type(query1.group_by(Sale.sid))
+
+    # EXPECTED_TYPE: GroupedQuery[Tuple[Customer, int], Tuple[Sale, Customer]]
+    reveal_type(query1.group_by(Customer, Sale.sid))
+
+    # fallback to Any if group_by is called with more than 5 arguments
+    # EXPECTED_TYPE: GroupedQuery[Any, Tuple[Sale, Customer]]
+    reveal_type(query1.group_by(Sale.sid, Sale.sid, Sale.sid, Sale.sid, Sale.sid, Sale.sid))
+
+    # EXPECTED_TYPE: GroupedQuery[Tuple[Customer, int], int]
+    reveal_type(query1.group_by(Customer, Sale.sid).select(Customer.cid))
+
+    # EXPECTED_TYPE: GroupedQuery[Tuple[Customer, int], Tuple[int, str]]
+    reveal_type(query1.group_by(Customer, Sale.sid).select(Customer.cid, Sale.item))
+
+    # fallback to Any if select is called with more than 5 arguments
+    s4 = query1.group_by(Sale).select(Sale.cid, Sale.cid, Sale.cid, Sale.cid, Sale.cid, Sale.cid)
+    # EXPECTED_TYPE: GroupedQuery[Sale, Any]
+    reveal_type(s4)
+
+
+def test_query_grouped_endpoints() -> None:
+    fb = FactBase()
+    query1 = fb.query(Sale, Customer).group_by(Customer, Sale.sid)
+
+    # EXPECTED_TYPE: Generator[Tuple[Tuple[Customer, int], Iterator[Tuple[Sale, Customer]]], None, None]
+    reveal_type(query1.all())
+
+    # EXPECTED_TYPE: Generator[Tuple[Tuple[Customer, int], Iterator[Customer]], None, None]
+    reveal_type(query1.select(Customer).all())
+
+    # EXPECTED_TYPE: Tuple[Customer, int], Iterator[Tuple[Sale, Customer]]]
+    reveal_type(query1.singleton())
+
+    # EXPECTED_TYPE: Tuple[Customer, int], Iterator[Tuple[Sale, Customer]]]
+    reveal_type(query1.first())
+
+    # EXPECTED_TYPE: Iterator[Tuple[Tuple[Customer, int], int]]
+    reveal_type(query1.count())
+
+    # EXPECTED_TYPE: int
+    reveal_type(query1.delete())
+
+
+def test_query_method_without_type_modification() -> None:
+
+    ungrouped = UnGroupedQuery[Tuple[int, int, str]]({}, QuerySpec())
+
+    # EXPECTED_TYPE: UnGroupedQuery[Tuple[int, int, str]]
+    reveal_type(ungrouped.where(Sale.cid == 3))
+
+    # EXPECTED_TYPE: UnGroupedQuery[Tuple[int, int, str]]
+    reveal_type(ungrouped.join(Sale.cid == Customer.cid))
+
+    # EXPECTED_TYPE: UnGroupedQuery[Tuple[int, int, str]]
+    reveal_type(ungrouped.heuristic(basic_join_order))
+
+    # EXPECTED_TYPE: UnGroupedQuery[Tuple[int, int, str]]
+    reveal_type(ungrouped.ordered())
+
+    # EXPECTED_TYPE: UnGroupedQuery[Tuple[int, int, str]]
+    reveal_type(ungrouped.distinct())
+
+    # EXPECTED_TYPE: UnGroupedQuery[Tuple[int, int, str]]
+    reveal_type(ungrouped.bind(ph_=42))
+
+
+    grouped = GroupedQuery[Tuple[int, str], Sale]({}, QuerySpec())
+
+    # EXPECTED_TYPE: GroupedQuery[Tuple[int, str], Sale]
+    reveal_type(grouped.where(Sale.cid == 3))
+
+    # EXPECTED_TYPE: GroupedQuery[Tuple[int, str], Sale]
+    reveal_type(grouped.join(Sale.cid == Customer.cid))
+
+    # EXPECTED_TYPE: GroupedQuery[Tuple[int, str], Sale]
+    reveal_type(grouped.heuristic(basic_join_order))
+
+    # EXPECTED_TYPE: GroupedQuery[Tuple[int, str], Sale]
+    reveal_type(grouped.ordered())
+
+    # EXPECTED_TYPE: GroupedQuery[Tuple[int, str], Sale]
+    reveal_type(grouped.distinct())
+
+    # EXPECTED_TYPE: GroupedQuery[Tuple[int, str], Sale]
+    reveal_type(grouped.bind(ph_=42))
+   
+def test_query_tuple() -> None:
+    # can't find a proper type annotation to handle BaseQueryImpl.tuple
+    # so for now we return BaseQueryImpl[Any] to get at least a type hint which is compatible to everything
+    ungrouped = UnGroupedQuery[Tuple[int, int, str]]({}, QuerySpec())
+
+    # EXPECTED_TYPE: BaseQueryImpl[Any]
+    reveal_type(ungrouped.tuple())
